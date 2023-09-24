cf381d5a0bc1ff2c1c812f334ae5d99e11c40a49
sahesh2003 <128036977+sahesh2003@users.noreply.github.com>
2023-09-24 13:49:07 +0330

    add CSS to App.css

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div class="container">
-        <div class="weather-app">
-          <form class="search-form" id="search-form">
-            <div class="row">
-              <div class="col-9">
+      <div className="container">
+        <div className="weather-app">
+          <form className="search-form" id="search-form">
+            <div className="row">
+              <div className="col-9">
                 <input
                   type="search"
                   placeholder="Type a city.."
-                  autofocus="on"
-                  autocomplete="off"
+                  autoFocus="on"
+                  autoComplete="off"
                   id="city-input"
-                  class="form-control shadow-sm input"
+                  className="form-control shadow-sm input"
                 />
               </div>
-              <div class="col-3">
+              <div className="col-3">
                 <input
                   type="submit"
                   value="Search"
-                  class="form-control btn btn-primary shadow-sm"
+                  className="form-control btn btn-primary shadow-sm"
                 />
               </div>
             </div>
@@ -32,35 +32,35 @@ function App() {
             <li id="date"></li>
             <li id="weatherName"></li>
           </ul>
-          <div class="row">
-            <div class="col-8">
-              <div class="row mt-3">
-                <div class="col-2 align-content-start" id="weather-img">
+          <div className="row">
+            <div className="col-8">
+              <div className="row mt-3">
+                <div className="col-2 align-content-start" id="weather-img">
                   <img
                     src=""
                     alt=""
-                    class="weather-icon float-left"
+                    className="weather-icon float-left"
                     id="weather-icon"
                   />
                 </div>
-                <div class="col-3 float-left mr-0" id="weather-tempreture">
-                  <span class="temperature" id="temperature"></span>
+                <div className="col-3 float-left mr-0" id="weather-tempreture">
+                  <span className="temperature" id="temperature"></span>
                 </div>
-                <div class="col-3 units">
+                <div className="col-3 units">
                   <span> °C</span>
                 </div>
               </div>
             </div>
-            <div class="col-4">
+            <div className="col-4">
               <ul>
                 <li id="humidity"></li>
                 <li id="wind"></li>
               </ul>
             </div>
           </div>
-          <div class="row" id="forecast"></div>
+          <div className="row" id="forecast"></div>
         </div>
-        <p class="weather-app link mt-0 pt-0" id="endNote">
+        <p className="weather-app link mt-0 pt-0" id="endNote">
           <a href="https://github.com/sahesh2003/SheCodesPlus-Github">
             Open-source code,
           </a>
